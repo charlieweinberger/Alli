bc6bff4859918771157232adaa0ac053822c86d0
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-02-01 15:40:27 -0800

    Adding a test

--- client/src/db/drizzle.ts
+++ client/src/db/drizzle.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { users } from "./schema";
-
+import * as schema from "./schema";
 const connectionString = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const client = postgres(connectionString);
-export const db = drizzle(client);
+export const db = drizzle(client, { schema: schema });
