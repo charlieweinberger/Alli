029142588392a0075cf8385586594e12eb037994
charlieweinberger <charlieweinberger05@gmail.com>
2025-02-02 13:21:15 -0800

    redesigned signup menu

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -10,16 +10,10 @@ interface inputType {
   required: boolean,
 }
 
-const inputList: inputType[] = [
+const inputColumn1: inputType[] = [
   {
     name: "username",
     placeholder: "Username",
-    type: "text",
-    required: true,
-  },
-  {
-    name: "name",
-    placeholder: "Full Name",
     type: "text",
     required: true,
   },
@@ -36,20 +30,28 @@ const inputList: inputType[] = [
     required: false,
   },
   {
-    name: "sexuality",
-    placeholder: "Sexual Orientation",
+    name: "hobbies",
+    placeholder: "Hobbies",
     type: "text",
     required: false,
   },
+];
+const inputColumn2: inputType[] = [
   {
-    name: "bio",
-    placeholder: "Bio",
-    type: "",
+    name: "name",
+    placeholder: "Full Name",
+    type: "text",
+    required: true,
+  },
+  {
+    name: "age",
+    placeholder: "Age",
+    type: "number",
     required: false,
   },
   {
-    name: "hobbies",
-    placeholder: "Hobbies",
+    name: "sexuality",
+    placeholder: "Sexual Orientation",
     type: "text",
     required: false,
   },
@@ -57,12 +59,6 @@ const inputList: inputType[] = [
     name: "major",
     placeholder: "Major",
     type: "text",
-    required: false,
-  },
-  {
-    name: "age",
-    placeholder: "Age",
-    type: "number",
     required: false,
   }
 ];
@@ -119,30 +115,58 @@ export default function SignUp() {
   };
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center py-6  h-[calc(100vh-56px)]">
       <form
         onSubmit={handleSubmit}
-        className="bg-white p-8 rounded-lg shadow-md w-96"
+        className="bg-white p-6 rounded-lg shadow-md flex flex-col gap-4"
       >
-        <h2 className="text-2xl font-bold mb-6 text-center">
+        <h2 className="text-2xl font-bold text-center">
           Sign Up
         </h2>
-        <div className="space-y-4">
-          {inputList.map(({ name, placeholder, type, required }: inputType, index: number) => {
-            return (
-              <div key={index}>
-                <input
-                  type={type}
-                  name={name}
-                  value={formData[name as keyof User]}
-                  onChange={handleChange}
-                  placeholder={placeholder}
-                  required={required}
-                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-rose-500"
-                />
-              </div>
-            );
-          })}
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row gap-4">
+            <div className="flex flex-col gap-4">
+              {inputColumn1.map(({ name, placeholder, type, required }: inputType, index: number) => {
+                return (
+                  <div key={index}>
+                    <input
+                      type={type}
+                      name={name}
+                      value={formData[name as keyof User]}
+                      onChange={handleChange}
+                      placeholder={placeholder}
+                      required={required}
+                      className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-rose-500"
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="flex flex-col gap-4">
+              {inputColumn2.map(({ name, placeholder, type, required }: inputType, index: number) => {
+                return (
+                  <div key={index}>
+                    <input
+                      type={type}
+                      name={name}
+                      value={formData[name as keyof User]}
+                      onChange={handleChange}
+                      placeholder={placeholder}
+                      required={required}
+                      className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-rose-500"
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <textarea 
+            name="bio"
+            value={formData["bio"]}
+            onChange={handleChange}
+            placeholder="Bio"
+            className="col-span-2 w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-rose-500"
+          />
         </div>
         <Button className="w-full">
           Sign In
