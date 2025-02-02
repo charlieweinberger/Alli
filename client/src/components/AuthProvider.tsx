37b465f364def6b06eb7a577f1375f7065e30c54
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-02-01 21:02:26 -0800

    adding an auth provider

---
+++ client/src/components/AuthProvider.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { User } from "@/lib/types";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface AuthContextType {
+  user: User | null;
+  login: (user: User) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (userData: User) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
