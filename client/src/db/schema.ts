32ccf0dd7237c6a3a05e0eab6f3b32d2bd5563eb
Christian Lasam <clasam@uci.edu>
2025-02-01 18:55:47 -0800

    Finalized database

--- client/src/db/schema.ts
+++ client/src/db/schema.ts
@@ -1,42 +1,37 @@
 import { pgTable, serial, text, timestamp, boolean, varchar, integer, enum as pgEnum} from "drizzle-orm/pg-core";
 
-export const genderIdentityEnum = pgEnum('gender_identity', ['male', 'female', 'non-binary', 'other']);
-export const sexualIdentityEnum = pgEnum('sexual_identity', ['heterosexual', 'homosexual', 'bisexual', 'pansexual', 'asexual', 'aroace', 'aromantic', 'demisexual', 'other']);
-
 export const users = pgTable('User', {
   userId: serial('userId').primaryKey(),
   username: text('username').notNull(),
-  password: text('password').notNull(),
+  // password: text('password').notNull(),
   name: text('name').notNull(),
   pronouns: text('pronouns').notNull(), 
   genderIdentity: text('genderIdentity'),
   sexuality: text('sexuality'),
   bio: text('bio'),
-  college: text('college'),
+  hobbies: text('hobbies'),
   major: text('major'), 
   age: integer('age'),
 });
 
-export const connectionStatusEnum = pgEnum('connection_status', ['pending', 'accepted', 'rejected']); 
+export const post = pgTable('Post', {
+  userId: integer('userId')
+    .notNull()
+    .references(() => users.userId),
+    postId: integer('postId').primaryKey(), 
+  title: text('title'),
+  description: text('description'),
+  createdAt: timestamp('created_at').defaultNow()
+});
 
 export const connection = pgTable('connections',{
   connectId: serial('connectId').primaryKey(),
-  poster: integer('poster')
+  user: integer('poster')
     .notNull()
     .references(() => users.userId),
   responder: integer('responder')
     .notNull()
     .references(() => users.userId),
-  status: connectionStatusEnum('status').default('pending'),
-});
-
-export const bucket = pgTable('Bucket', {
-  userId: serial('userIJd')
-    .notNull()
-    .references(() => users.userId),
-  bucketId: serial("bucketId").primaryKey(),
-  bucketName: text('bucketName'),
-  profilePicture: text('profilePicture')
 });
 
 export const messages = pgTable('messages', {
